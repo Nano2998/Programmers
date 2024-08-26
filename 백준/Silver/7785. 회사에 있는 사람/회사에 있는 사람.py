a76a2e1d494085c24b6d18f743c0b97967da3975
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-08-26 16:41:40 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 252 ms, Memory: 144852 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = {}
+lis = []
+
+for _ in range(n):
+    peoples = list(map(str,input().split()))
+    a[peoples[0]] = peoples[1]
+
+
+for key, value in a.items():
+    if value == 'enter':
+        lis.append(key)
+
+lis.sort(reverse=True)
+
+for i in lis:
+    print(i)
