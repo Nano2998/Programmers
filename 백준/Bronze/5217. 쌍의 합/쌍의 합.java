430edd71eb8277713579074ec0719bfe5feb846d
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-10-25 16:34:24 +0900

    [Bronze III] Title: 쌍의 합, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.java
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.java
@@ -4,31 +4,32 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine()); // 테스트 케이스 수
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++){
             int M = Integer.parseInt(br.readLine()); // 입력하는 숫자
-            System.out.print("Pairs for " + M + ": ");
-            
-            boolean firstPair = true;
-            for (int j = 1; j < M; j++) {
-                for (int k = j + 1; k < M; k++) {
-                    if (j + k == M) {
-                        if (!firstPair) {
-                            System.out.print(", ");
+            System.out.print("Pairs for " + M +": ");
+            for (int j = 0; j < M; j++){
+                for (int k = 0; k < M; k++) {
+                    if (j < k) {
+                        if (j < 2) {
+                            if (j + k == M) {
+                                System.out.print(j);
+                                System.out.print(" " + k);
+                            }
+                        } else {
+                            if (j + k == M) {
+                                System.out.print(", " + j);
+                                System.out.print(" " + k);
+                            }
                         }
-                        System.out.print(j + " " + k);
-                        firstPair = false;
                     }
                 }
             }
             System.out.println();
         }
-        
-        br.close();
-        bw.close();
     }
 }
