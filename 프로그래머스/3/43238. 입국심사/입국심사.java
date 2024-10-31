2d72a3709e9273c40b1eb9fc8c2508a0e7d04ede
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-10-31 09:58:59 +0900

    [level 3] Title: 입국심사, Time: 143.66 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        
+        Arrays.sort(times);
+        
         long start = 0L;
         long end = 1_000_000_000 * 1_000_000_000L;
         long answer = 0;
-        Arrays.sort(times);
+        
         while(start <= end) {
             long mid = (start + end) / 2;
             long result = 0;
+            
             for(long time : times) {
                 result += mid / time;
             }
