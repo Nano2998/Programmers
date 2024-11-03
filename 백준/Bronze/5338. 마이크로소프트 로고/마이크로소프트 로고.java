e844f02f55b9c47ab4565fe1a25cf780230aaa64
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-11-03 22:21:04 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        String a ="       _.-;;-._";
+        String b = "'-..-'|   ||   |";
+        String c = "'-..-'|_.-;;-._|";
+        String d ="'-..-'|   ||   |";
+        String f ="'-..-'|_.-''-._|";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
