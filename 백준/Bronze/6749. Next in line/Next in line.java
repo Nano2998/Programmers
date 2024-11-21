a77172547557779893e81bfe0cf6035be3af44b2
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-11-21 22:37:47 +0900

    [Bronze IV] Title: Next in line, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int a = m - n;
+
+        System.out.println(m+a);
+
+    }
+}
