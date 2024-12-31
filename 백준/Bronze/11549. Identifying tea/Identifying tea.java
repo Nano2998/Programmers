4179e9ed30d5ee95a0a7fc4f59070a0d8281ba6f
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-12-31 23:27:34 +0900

    [Bronze IV] Title: Identifying tea, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int[] arr = new int[5];
+        int cnt = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] == t) {
+                cnt += 1;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
