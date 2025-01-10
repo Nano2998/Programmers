1775e024df75c55ac4f22fa4147006b541084ec5
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2025-01-10 23:47:35 +0900

    [Bronze V] Title: CAPS, Time: 268 ms, Memory: 21156 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String aCase = a.toUpperCase();
+        System.out.println(aCase);
+    }
+}
