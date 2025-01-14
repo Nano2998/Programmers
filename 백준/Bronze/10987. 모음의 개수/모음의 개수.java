26b8ac975e1a4cd551c626a9509385057d36a739
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2025-01-14 23:46:45 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        int cnt = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u') {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
