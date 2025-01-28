e9d01550c88975ce2af00ca3328b1a2cbe45585d
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2025-01-28 22:10:11 +0900

    [Bronze V] Title: Site Score, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()) * 56 +Integer.parseInt(st.nextToken()) * 24 + Integer.parseInt(st.nextToken()) * 14 + Integer.parseInt(st.nextToken()) * 6 );
+
+    }
+}
