70aad39792ebd4ee52f764cb67cef801f98dc801
Nano2998 <gytjs2998@naver.com>
2025-02-24 21:50:24 +0900

    [Bronze V] Title: SciComLove, Time: 96 ms, Memory: 14132 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String s = "SciComLove";
+        for (int i = 0; i < n; i++) {
+            System.out.println(s);
+        }
+    }
+}
