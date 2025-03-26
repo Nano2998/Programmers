30d9ef4e7e75eaf0ce238a15b9f382bdc69e0a10
Nano2998 <gytjs2998@naver.com>
2025-03-26 22:41:09 +0900

    [Bronze V] Title: 세금, Time: 124 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int i = Integer.parseInt(br.readLine());
+
+        int n = i - (i * 22 / 100);
+
+        int m = i - (i*20/100*22/100);
+        System.out.println(n + " " + m);
+
+    }
+}
