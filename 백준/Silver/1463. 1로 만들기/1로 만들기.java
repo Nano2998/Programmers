6f02871fc5f131507904f0f05aacffd2f383d43c
Nano2998 <gytjs2998@naver.com>
2025-04-02 00:01:44 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,27 +1,20 @@
 import java.io.*;
-import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static int n;
+    static int[] d;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        int[] dp = new int[N +1];
+        n = Integer.parseInt(br.readLine());
+        d = new int[n + 1];
 
-        dp[0] = 0;
-        dp[1] = 0;
-
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            if(i%2 ==0) d[i] = Math.min(d[i], d[i / 2] + 1);
+            if(i%3 ==0) d[i] = Math.min(d[i], d[i / 3] + 1);
         }
-        System.out.println(dp[N]);
+
+        System.out.println(d[n]);
     }
 }
